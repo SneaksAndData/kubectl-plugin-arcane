0d3eb2f99a7c065954b57188541254c40ea7c1b0
Vitalii Savitskii <visa@ecco.com>
2026-02-11 11:34:35 +0100

    WIP

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{
-		Use:   "my-app",
-		Short: "A brief description",
+	// 1. Initialize standard flags (kubeconfig, namespace, etc.)
+	configFlags := genericclioptions.NewConfigFlags(true)
+
+	cmd := &cobra.Command{
+		Use: "kubectl-myplugin",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Code that runs when no subcommand is provided
+			// 2. Generate REST config from flags
+			config, _ := configFlags.ToRESTConfig()
+
+			// 3. Create a Kubernetes client
+			clientset, _ := kubernetes.NewForConfig(config)
+			if clientset != nil {
+				fmt.Println("hello")
+			}
+
+			// Your logic here (e.g., list pods)
 		},
 	}
-	configFlags := genericclioptions.NewConfigFlags(true)
-	configFlags.AddFlags(rootCmd.PersistentFlags())
 
-	fx.Supply(configFlags)
+	// 4. Register the flags with your command
+	configFlags.AddFlags(cmd.Flags())
+	cobra.CheckErr(cmd.Execute())
+}
+
+func AsCommand(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(commands.CobraCommand)),
+		fx.ResultTags(`group:"commands"`),
+	)
 }

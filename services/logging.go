1da01596209754a35f042fc8bf6344063441d8a9
Vitalii Savitskii <visa@ecco.com>
2026-03-09 11:05:02 +0100

    Ignore coverage in logging.go

--- services/logging.go
+++ services/logging.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func logError(object PrintableObject, operation string, cause error) {
+func logError(object PrintableObject, operation string, cause error) { // coverage-ignore
 	name := FormatName(object)
 	_, err := fmt.Fprintf(os.Stderr, "%s Failed %s: %v\n", name, operation, cause)
 	if err != nil {

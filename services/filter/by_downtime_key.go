416c2e450a848935b60e28f7259cad5aed9698d4
Vitalii Savitskii <visa@ecco.com>
2026-03-17 15:10:25 +0100

    Refactor object listing in the downtime service (#56)
    * Refactor object listing in the downtime service
    
    * Fix linter

---
+++ services/filter/by_downtime_key.go
@@ -0,0 +1,22 @@
+package filter
+
+import (
+	"github.com/SneaksAndData/arcane-operator/services/controllers/stream"
+	"github.com/sneaksAndData/kubectl-plugin-arcane/services/interfaces"
+)
+
+var _ interfaces.ObjectFilter = (*ByDowntimeKey)(nil)
+
+type ByDowntimeKey struct {
+	key string
+}
+
+func NewByDowntimeKey(key string) *ByDowntimeKey {
+	return &ByDowntimeKey{
+		key: key,
+	}
+}
+
+func (f *ByDowntimeKey) Matches(definition stream.Definition) (bool, error) {
+	return definition.ToUnstructured().GetLabels()["arcane.sneaksanddata.com/downtime"] == f.key, nil
+}

fcd6ca9e800e8be1abb1e023843f370da8d3f3a2
Vitalii Savitskii <visa@ecco.com>
2026-03-18 13:00:55 +0100

    Implement listing service

---
+++ services/downtime_summarization_processor.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"context"
+
+	v1 "github.com/SneaksAndData/arcane-operator/pkg/apis/streaming/v1"
+	"github.com/sneaksAndData/kubectl-plugin-arcane/logging"
+	"github.com/sneaksAndData/kubectl-plugin-arcane/services/interfaces"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ interfaces.UnstructuredProcessor = (*DowntimeSummarizationProcessor)(nil)
+
+type DowntimeSummarizationProcessor struct {
+	reader  interfaces.UnstructuredReader
+	Summary map[string]int
+}
+
+func NewDowntimeSummarizationProcessor(reader interfaces.UnstructuredReader) *DowntimeSummarizationProcessor {
+	return &DowntimeSummarizationProcessor{
+		reader:  reader,
+		Summary: make(map[string]int),
+	}
+}
+
+func (s DowntimeSummarizationProcessor) Process(ctx context.Context, def types.NamespacedName, class *v1.StreamClass) (*unstructured.Unstructured, bool, error) {
+	stream, err := s.reader.Read(ctx, class, def)
+	if err != nil { // coverage-ignore
+		return nil, false, err
+	}
+
+	labels := stream.GetLabels()
+
+	if labels == nil {
+		logging.LogError(stream, "has no labels, skipping", err)
+		return nil, false, nil // Skip items that have no labels
+	}
+
+	s.Summary[labels["arcane.sneaksanddata.com/downtime"]]++
+
+	// We return nil here because we don't want to modify the original object, we just want to update our summaries
+	return nil, false, nil
+}
---
+++ services/publisher/all_stream_definitions.go
@@ -0,0 +1,44 @@
+package publisher
+
+import (
+	"context"
+
+	cmdinterfaces "github.com/sneaksAndData/kubectl-plugin-arcane/commands/interfaces"
+	"github.com/sneaksAndData/kubectl-plugin-arcane/services/filter"
+	"github.com/sneaksAndData/kubectl-plugin-arcane/services/interfaces"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ interfaces.QueuePublisher = (*AllStreamDefinitions)(nil)
+
+type AllStreamDefinitions struct {
+	provider cmdinterfaces.ClientProvider
+}
+
+func NewAllStreamDefinitionsPublisher(provider cmdinterfaces.ClientProvider) *AllStreamDefinitions {
+	return &AllStreamDefinitions{
+		provider: provider,
+	}
+}
+
+func (a AllStreamDefinitions) PublishStreamDefinitions(ctx context.Context, target interfaces.Queue) error {
+	client, err := a.provider.ProvideClientSet()
+	if err != nil {
+		return err
+	}
+
+	streamClasses, err := client.StreamingV1().StreamClasses("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, sc := range streamClasses.Items {
+		queuePublisher := NewStreamClassMembersPublisher(a.provider, sc.Name, "", filter.NewAllowAll())
+		err = queuePublisher.PublishStreamDefinitions(ctx, target)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
